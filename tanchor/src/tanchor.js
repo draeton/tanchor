f60e08d7d7e6cc31757b46afb279d1c8b61ff557
Matthew Cobbs <draeton@gmail.com>
2011-12-02 01:30:03 -0500

    Copied files from master branch

---
+++ tanchor/src/tanchor.js
@@ -0,0 +1,14 @@
+// ## tanchor
+// 
+// 
+// [http://draeton.github.com/tanchor](http://draeton.github.com/tanchor)
+//
+// Copyright 2011, Matthew Cobbs  
+// Licensed under the MIT license.
+//
+/*global jQuery*/
+(function (window, $) {
+
+    "use strict";
+
+})(window, jQuery);
