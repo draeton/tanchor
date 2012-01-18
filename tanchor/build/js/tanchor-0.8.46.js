910eb36d1ae91316dcbf5a987e16aadf15cb1faa
Matthew Cobbs <draeton@gmail.com>
2012-01-17 21:06:25 -0500

    Copied files from master branch

--- tanchor/build/js/tanchor-0.8.46.js
+++ tanchor/build/js/tanchor-0.8.46.js
@@ -255,13 +255,24 @@ var Tanchor = (function (window, document) {
     };
   };
 
+  /* fix for pathname leading slash in IE */
+  var pathnameGetter = function () {
+    var val = this.anchor.pathname;
+
+    if (!/^\//.test(val)) {
+      val = "/" + val;
+    }
+
+    return val;
+  };
+
   var nativeMethods = (function () {
     var methods = {}, props, prop, i, l;
 
     props = ["href", "protocol", "host", "hostname", "port", "pathname", "search", "hash"];
     for (i = 0, l = props.length; i < l; i++) {
       prop = props[i];
-      methods[prop] = nativeGetter(prop);
+      methods[prop] = (prop === "pathname") ? pathnameGetter : nativeGetter(prop);
     }
 
     return methods;
